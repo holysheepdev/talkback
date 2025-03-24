1d74a7712ae4eee7f69434287eba49d378672f59
Alex Trocan <alexteo.trocan@gmail.com>
2025-03-24 22:15:28 +0200

    figured out tailwind config and added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DefaultNavbarComponent } from '../components/navbars/default-navbar/default-navbar.component';
+import { LandingNavbarComponent } from '../components/navbars/landing-navbar/landing-navbar.component';
+import { NavbarComponent } from '../components/navbars/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/components/navbars/default-navbar/default-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-navbar',
+  imports: [],
+  templateUrl: './default-navbar.component.html',
+  styleUrl: './default-navbar.component.scss'
+})
+export class DefaultNavbarComponent {
+
+}
