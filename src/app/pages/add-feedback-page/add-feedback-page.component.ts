1aaf4e2cfb2a50a95f6806cde04eccb8cd8ca227
Alex Trocan <alexteo.trocan@gmail.com>
2025-03-31 09:13:57 +0300

    pdokfs

--- src/app/pages/add-feedback-page/add-feedback-page.component.ts
+++ src/app/pages/add-feedback-page/add-feedback-page.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { SupabaseService } from '../../services/supabase.service';
 
 @Component({
   selector: 'app-add-feedback-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './add-feedback-page.component.html',
-  styleUrl: './add-feedback-page.component.scss'
+  styleUrl: './add-feedback-page.component.scss',
 })
 export class AddFeedbackPageComponent {
+  feedbackForm = inject(FormBuilder).group({
+    title: '',
+    category: '',
+    description: '',
+    status: '',
+  });
+  private supabase = inject(SupabaseService);
 
+  session: any;
+  loading = false;
+  signOut() {}
+  submitFeedback() {}
 }
